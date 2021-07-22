836d0555ed80cb905d4b5b7e5c5c9efe9e9d9b9c
rapando <samsonrapando@gmail.com>
2021-07-22 23:21:28 +0300

    added assembly

--- 005-assembly/01.hello.asm
+++ 005-assembly/01.hello.asm
@@ -8,17 +8,17 @@
 ; syntax : [label] mnemonic [operands] [;comment]
 
 section .text
-    global _start   ; must be declared for linker (ld)
+    global _start           ; must be declared for linker (ld)
 
-_start:             ; tells the linker the starting point
-    mov edx, len    ; message length
-    mov ecx, msg    ; message to write
-    mov ebx, 1      ; file descriptor (stdout)
-    mov eax, 4      ; system call number (sys_write)
-    int 0x80        ; call kernel
+_start:                     ; tells the linker the starting point
+    mov edx, len            ; message length
+    mov ecx, msg            ; message to write
+    mov ebx, 1              ; file descriptor (stdout)
+    mov eax, 4              ; system call number (sys_write)
+    int 0x80                ; call kernel
 
-    mov eax, 1      ; system call number (sys_exit)
-    int 0x80        ; call kernel
+    mov eax, 1              ; system call number (sys_exit)
+    int 0x80                ; call kernel
 
 section .data
 msg db 'Hello, world', 0xa  ; string to be printed
